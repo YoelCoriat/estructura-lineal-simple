c4b97c1b5a6f33d37852903df346b51d9a8426aa
Yoel <yoel.coriat@uip.edu.pa>
2026-03-10 15:52:36 -0500

    Subir main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+nombre = input("¿Cual es su nombre?\n")
+edad = input("\n¿Cuantos años tiene?\n")
+residencia = input("\n¿Cual es su residencia actual?\n")
+
+# Por favor realizar los cambios aquí, y añadir 2 líneas como mínimo por cada uno
+print(f"¡Saludos a {nombre}! Este es el nombre del usuario, y tiene {edad} años de edad. "
+      f"El usuario reside actualmente en {residencia}, aunque actualmente no se encuentra en su residencia, si no en Paris.")
