64463df3dfbc2476722506beb9f8c80a435a9aba
Maryane Holt <maryane.holt@uip.edu.pa>
2026-03-11 10:45:25 -0500

    Añadida variables de entrada e lineas de salida
    Se agrego dos preguntas para recopilar informacion para la ejecucion del programa

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 nombre = input("¿Cual es su nombre?\n")
 edad = input("\n¿Cuantos años tiene?\n")
 residencia = input("\n¿Cual es su residencia actual?\n")
+hobby = input("\n¿Que te gusta hacer en tiempo libre?\n")
+amigo = input("\n¿Quien es tu mejor amigo?\n")
 profesion = input("\n¿A qué te dedicas o qué estudias?\n")
 suenio = input("\n¿Cuál es uno de tus mayores sueños?\n")
+
 # Por favor realizar los cambios aquí, y añadir 2 líneas como mínimo por cada uno
 print(f"\n¡Saludos a {nombre}! Este es el nombre del usuario, y tiene {edad} años de edad. "
       f"El usuario reside actualmente en {residencia}, aunque actualmente no se encuentra en su residencia, si no en Paris.")
+print(f"El usuario no suele tener mucho tiempo libre, ya que labora como {profesion}.)
+print(f"Pero cuando el usuario encuentra tiempo, le encanta ir a {hobby} con {amigo}.")
 print(f"Un día, {nombre}, de {edad} años y originario de {residencia}, decidió viajar por el mundo mientras desarrollaba su pasión por {profesion}.")
 print(f"Durante su estancia en París, {nombre} recuerda que su mayor sueño es {suenio}.")
 print(f"Así comienza la historia de {nombre}, una persona curiosa que salió de {residencia} para perseguir su sueño.")
