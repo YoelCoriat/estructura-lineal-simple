de034afb33c2b521446806393ddaec19a64e4d48
Yoel Coriat <yoel.coriat@uip.edu.pa>
2026-03-11 11:18:10 -0500

    Update main.py
    Corregir algunas incongruencias de sintaxis entre diferentes colaboradores

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ profesion = input("\n¿A qué te dedicas o qué estudias?\n")
 suenio = input("\n¿Cuál es uno de tus mayores sueños?\n")
 
 # Por favor realizar los cambios aquí, y añadir 2 líneas como mínimo por cada uno
-print(f"\n¡Saludos a {nombre}! Este es el nombre del usuario, y tiene {edad} años de edad. "
-      f"El usuario reside actualmente en {residencia}, aunque actualmente no se encuentra en su residencia, si no en Paris.")
+print(f"\n¡Saludos a {nombre}! Este es el nombre del usuario, y tiene {edad} años de edad. ")
+print(f"El usuario reside actualmente en {residencia}, aunque actualmente no se encuentra en su residencia, si no en Paris.")
 print(f"El usuario no suele tener mucho tiempo libre, ya que labora como {profesion}.")
 print(f"Pero cuando el usuario encuentra tiempo, le encanta ir a {hobby} con {amigo}.")
 print(f"Un día, {nombre}, de {edad} años y originario de {residencia}, decidió viajar por el mundo mientras desarrollaba su pasión por {profesion}.")
